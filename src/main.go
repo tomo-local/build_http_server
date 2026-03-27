4e4bf7831e1e3ce0727496e6e539a9bf48266495
tomo-local <tomohiro.togashi@gmail.com>
2026-03-28 00:44:20 +0900

    feat: TCPのコネクションの確立と受け入れが可能

--- src/main.go
+++ src/main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(buf[:n]))
+
+	if _, err := conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!")); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
